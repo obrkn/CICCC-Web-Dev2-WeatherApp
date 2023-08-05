c77bdedd5424f8bf71cc31ab28a4c4a2e0a93064
obrkn <k33mdukb9n@yahoo.co.jp>
2023-08-04 19:34:25 -0700

    jsファイルの小分け

--- js/api_test.js
+++ js/api_test.js
@@ -1,9 +1,12 @@
 require('dotenv').config()
 const API_KEY = process.env.WEATHER_API_KEY;
 
+const VANCOUVER_LAT = '49.2827';
+const VANCOUVER_LON = '-123.1207';
+
 const ApiTest = async () => {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Tokyo&units=metric&APPID=${API_KEY}`);
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${VANCOUVER_LAT}&lon=${VANCOUVER_LON}&APPID=${API_KEY}`);
     const data = await response.json();
     console.log(data);
   } catch (error) {
