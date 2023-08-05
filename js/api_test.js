c70b82e3fd6ec66ed6c51d0769fa662051682cde
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-08-04 23:22:26 -0700

    add prettier,eslint,stylelint

--- js/api_test.js
+++ js/api_test.js
@@ -1,17 +1,19 @@
-const API_KEY = config.WEATHER_API_KEY;
+const API_KEY = process.env.API_KEY;
 
-const VANCOUVER_LAT = '49.2827';
-const VANCOUVER_LON = '-123.1207';
+const VANCOUVER_LAT = "49.2827";
+const VANCOUVER_LON = "-123.1207";
 
 const ApiTest = async () => {
-  try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${VANCOUVER_LAT}&lon=${VANCOUVER_LON}&APPID=${API_KEY}`);
-    const data = await response.json();
-    console.log(data);
-    document.getElementById('api-test').innerHTML = JSON.stringify(data);
-  } catch (error) {
-    console.log(error);
-  }
+    try {
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${VANCOUVER_LAT}&lon=${VANCOUVER_LON}&APPID=${API_KEY}`,
+        );
+        const data = await response.json();
+        console.log(data);
+        document.getElementById("api-test").innerHTML = JSON.stringify(data);
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 setTimeout(ApiTest, 1000);
