d050ecd5f170b2c1f973b4f6b2e1c0220fa4e922
orange-na <d-0133@tsuyama.kosen-ac.jp>
2023-08-06 19:18:27 -0700

    🐛 fix bug in sass

--- js/current_weather.js
+++ js/current_weather.js
@@ -1,19 +1,20 @@
 // テスト用
 // console.log(CurrentWeather('49.2827', '-123.1207'));
 
-const latitude = '49.2827';
-const longtitude = '-123.1207';
-const API_KEY = '9c6c9f9d647a782c8d910a14542ffff5';
-
+const latitude = "49.2827";
+const longtitude = "-123.1207";
+const API_KEY = "9c6c9f9d647a782c8d910a14542ffff5";
 
 const getCurrentWeather = async () => {
-       try {
-              const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${API_KEY}`);
-              const res = await data.json();
-              console.log(res);
-       } catch (err) {
-              console.log(err); 
-       }
-}
+    try {
+        const data = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longtitude}&appid=${API_KEY}`,
+        );
+        const res = await data.json();
+        console.log(res);
+    } catch (err) {
+        console.log(err);
+    }
+};
 
 getCurrentWeather();
