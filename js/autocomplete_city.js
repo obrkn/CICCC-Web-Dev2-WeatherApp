918ce327dda882b38ef8a1aed82bde975a515020
Yo-mah-Ya <shanzhongziye@gmail.com>
2023-08-08 20:15:48 -0700

    implemented onPlaceChanged

--- js/autocomplete_city.js
+++ js/autocomplete_city.js
@@ -3,12 +3,35 @@ let autocomplete;
 function onPlaceChanged() {
     const place = autocomplete.getPlace();
     document.getElementById("city-name").innerHTML = place.name;
-    document.getElementById("city-lat").innerHTML = place.geometry.location
-        .lat()
-        .toFixed(4);
-    document.getElementById("city-lon").innerHTML = place.geometry.location
-        .lng()
-        .toFixed(4);
+    const lat = place.geometry.location.lat().toFixed(4);
+    document.getElementById("city-lat").innerHTML = lat;
+    const lon = place.geometry.location.lng().toFixed(4);
+    document.getElementById("city-lon").innerHTML = lon;
+
+    // eslint-disable-next-line no-undef
+    getCurrentWeather({
+        latitude: lat,
+        longitude: lon,
+    }).catch((error) => {
+        console.error(error);
+    });
+
+    // eslint-disable-next-line no-undef
+    forecastDailyApi({
+        lat,
+        lon,
+    }).catch((error) => {
+        console.error(error);
+    });
+
+    // eslint-disable-next-line no-undef
+    forecastHourlyApi({
+        lat,
+        lon,
+        targetIndex: 0,
+    }).catch((error) => {
+        console.error(error);
+    });
 }
 
 // eslint-disable-next-line no-unused-vars
