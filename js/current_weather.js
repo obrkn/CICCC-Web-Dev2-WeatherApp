cdb3f69caa1cd3af81939ab7ffbd835a3d4c1145
orange-na <d-0133@tsuyama.kosen-ac.jp>
2023-08-09 19:21:31 -0700

    ✨ change the current weather following a city name

--- js/current_weather.js
+++ js/current_weather.js
@@ -8,10 +8,21 @@ const getCurrentWeather = async ({ latitude, longitude }) => {
         const API_KEY = "9c6c9f9d647a782c8d910a14542ffff5";
 
         const data = await fetch(
-            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}`,
+            `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=imperial&appid=${API_KEY}`,
         );
         const res = await data.json();
-        console.log(res);
+        const weatherIcon = res.weather[0].icon;
+        const iconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+        const card = document.querySelector("#current .card");
+        const img = card.querySelector("img");
+        const temp = card.querySelector(".temp");
+        const humidity = card.querySelector(".humidity");
+        const wind = card.querySelector(".wind");
+
+        img.setAttribute("src", iconUrl);
+        temp.textContent = Math.round(res.main.temp) + "°F";
+        humidity.textContent = Math.round(res.main.humidity) + "%";
+        wind.textContent = Math.round(res.wind.speed) + "mph";
     } catch (err) {
         console.log(err);
     }
